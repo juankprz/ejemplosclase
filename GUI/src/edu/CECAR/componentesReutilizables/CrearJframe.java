d4f2bc63bae5252993bb5b376c3c101a66ace262
1103220136 <1103220136@SJLBA0111B15.admcecar.local>
2017-03-21 16:46:38 -0500

    GUI reutilizable commit 2

--- GUI/src/edu/CECAR/componentesReutilizables/CrearJframe.java
+++ GUI/src/edu/CECAR/componentesReutilizables/CrearJframe.java
@@ -12,6 +12,7 @@ public abstract class CrearJframe {
     /**
      * Metodo que define la GUI estandar de mi empresa 
      */
+    private  JFrame frame;
     public CrearJframe() {
       this("", 800,600);
     }
@@ -21,11 +22,16 @@ public abstract class CrearJframe {
        
     }
      public CrearJframe(String titulo,int ancho, int alto) {
-       JFrame frame = new JFrame(titulo); 
-       frame.setSize(800,600);
+       frame = new JFrame(titulo); 
+       frame.setSize(ancho,alto);
        frame.setLocationRelativeTo(null);
        frame.setLayout(null);
        
     }
+     public void mostrarJFrame(){
+       frame.setVisible(true);
+     }
+    
+    
 
 }
---
+++ GUI/src/edu/CECAR/presentacion/GUIEstudiante.java
@@ -0,0 +1,20 @@
+package edu.CECAR.presentacion;
+
+import edu.CECAR.componentesReutilizables.CrearJframe;
+
+/**
+ *
+ * @author:1103220136
+ * @Fecha creacion:21/03/2017
+ *
+ */
+public class  GUIEstudiante extends  CrearJframe{
+
+    public GUIEstudiante() {
+        mostrarJFrame();
+    }
+   public static void main(String[] args) {
+        new GUIEstudiante();
+    }
+    
+}
